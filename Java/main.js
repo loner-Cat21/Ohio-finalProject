7868f1441cd736aa443eaca0212d4fe20d505d51
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2023-05-17 12:48:05 -0400

    assddgd

--- Java/main.js
+++ Java/main.js
@@ -7,4 +7,35 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+//   <div class="weatherContainer">
+//   <h3>Weather in Columbus</h3>
+//   <p id="currentTemp">temperature: </p>
+//   <img id="iconWeather">
+//   <div class="temp">
+//     <p id="minTemp"></p>
+//     <p id="maxTemp"></p>
+//   </div>
+// </div>
+   let weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=columbus&appid=c8b260397fd294145748ea4206128b3f';
+   let TempMin = document.getElementById('minTemp');
+   let TempMax = document.getElementById('maxTemp');
+   let CurrentTemp = document.getElementById('currentTemp');
+   let imgIcon = document.getElementById('iconWeather');
+
+   function getInfo() {
+    fetch(weatherAPI)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      TempMin.textContent+=Number((data.main[0].temp_min - 273.15)+33.8).toFixed(5)+"°C";
+      TempMax.textContent+=Number((data.list[0].main.temp_max - 273.15)+33.8).toFixed(5)+"°C";
+      let icon = data.list[0].weather[0].icon;
+      imgIcon.src ="https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+    })
+    .catch(err => alert("something went wrong: Please check your internet connection"))
+  
+  }
+
+
   
